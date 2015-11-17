c3a5e688966b0d597c0e26669f84acade1642595
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-11-17 19:33:31 +0100

    chore(docs): fix typo

--- activate.php
+++ activate.php
@@ -1,6 +1,6 @@
 <?php
 
 if (!is_callable('elgg_view_input')) {
-	register_error('"Registration Form" relies on API that has not yet been included into core. Please download and enable "Forms API" plugin that provides temporary wrappers.');
+	register_error('"SEO Tools" relies on API that has not yet been included into core. Please download and enable "Forms API" plugin that provides temporary wrappers.');
 	return false;
 }
