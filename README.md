5d2af2fdad31a651e7bbf8144e5d1dd0611b76a7
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-12-10 11:06:03 +0100

    feat(releases): improved automated releases and dependency management

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 SEO Tools for Elgg
-============
+==================
 ![Elgg 2.0](https://img.shields.io/badge/Elgg-2.0.x-orange.svg?style=flat-square)
 
 ## Features
--- activate.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if (!is_callable('elgg_view_input')) {
-	register_error('"SEO Tools" relies on API that has not yet been included into core. Please download and enable "Forms API" plugin that provides temporary wrappers.');
-	return false;
-}
